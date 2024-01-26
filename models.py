df98b80eb0d87dc22fb9a1c22da714c9a0f1b5eb
ajackwere <austinewere59@gmail.com>
2024-01-26 13:18:47 +0300

    Modified model by adding status for agent assignments

--- models.py
+++ models.py
@@ -11,9 +11,11 @@ class Message(db.Model, SerializerMixin):
     user_id = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, server_default=db.func.now())
     message_body = db.Column(db.Text)
+    status = db.Column(db.String(20), default='unassigned')
+
 
     def __repr__(self):
-        return f'<Message {self.id}>'
+        return f'<Message {self.id} - Status: {self.status}>'
     
     def serialize(self):
         return {
@@ -21,4 +23,5 @@ class Message(db.Model, SerializerMixin):
             'user_id': self.user_id,
             'timestamp': self.timestamp.isoformat(),
             'message_body': self.message_body,
+            'status': self.status,
         }
